9110f57db606eaa5e92dcff449de6f4a06b07d10
Merajul Islam <buildwithmeraj@gmail.com>
2026-02-27 15:06:26 +0600

    made a skeleton layout of the dashboard using grid

--- src/components/pages/dashboard/Dashboard.jsx
+++ src/components/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div className="min-h-screen grid grid-cols-14 ">
+      <div className="bg-base-300 col-span-3 m-4 rounded-3xl"></div>
+      <div className="col-span-11 my-4 mr-4">
+        <div className="h-28 bg-base-300 rounded-3xl"></div>
+        <div className="h-[calc(100vh-10rem)] bg-base-300 rounded-3xl mt-4"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
